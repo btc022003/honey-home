78e64e55120d20c25c313fb7ef2b8114011a272f
Arivin <btc022003@gmail.com>
2021-05-11 15:59:44 +0800

    修复文件上传接口返回路径异常的问题

--- honey-home-server/api/controllers/api/v1/CommonController.js
+++ honey-home-server/api/controllers/api/v1/CommonController.js
@@ -4,7 +4,7 @@
  * @description :: Server-side actions for handling incoming requests.
  * @help        :: See https://sailsjs.com/docs/concepts/actions
  */
-
+const path = require("path");
 module.exports = {
   // 文件上传功能，上传之后需要吧uploads目录设置成静态资源目录
   uploadFile: function (req, res) {
@@ -15,7 +15,7 @@ module.exports = {
       // console.log(files[0]);
       return res.json({
         code: 1,
-        data: "/uploads/" + files[0].fd.split("/").splice(-1)[0],
+        data: "/uploads/" + path.basename(files[0].fd),
       });
     });
   },
