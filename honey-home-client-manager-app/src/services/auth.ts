21853937227c29647e753f5aa2883d134e38d9d2
Arivin <btc022003@gmail.com>
2021-07-02 06:04:33 +0800

    代码格式化

--- honey-home-client-manager-app/src/services/auth.ts
+++ honey-home-client-manager-app/src/services/auth.ts
@@ -5,18 +5,15 @@ import { request } from 'umi';
  * @param data
  * @returns
  */
-export async function login(data: {
-  userName:string;
-  password: string;
-}) {
+export async function login(data: { userName: string; password: string }) {
   return request('/api/v1/auth/manager_login', {
     method: 'post',
-    data
+    data,
   });
 }
 /**
  * 获取当前用户
  */
 export async function currentUser() {
-  return request('/api/v1/auth/manager_info')
+  return request('/api/v1/auth/manager_info');
 }
